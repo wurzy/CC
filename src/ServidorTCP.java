01da2ef98299d2ef99612234492020b9af14c3a8
Válter <47797461+wurzy@users.noreply.github.com>
2020-04-18 19:24:27 +0100

    updated with echo server

--- src/ServidorTCP.java
+++ src/ServidorTCP.java
@@ -1,3 +1,20 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServidorTCP {
 
+    public static void main(String... args) throws Exception {
+        ServerSocket sSocket = new ServerSocket(12345);
+
+        while (true) {
+            Socket clSock = sSocket.accept();
+
+            WorkerTCP worker = new WorkerTCP(clSock);
+
+            System.out.println("Assigning new thread to new client");
+
+            Thread newClient = new Thread(worker);
+            newClient.start();
+        }
+    }
 }
--- src/WorkerTCP.java
+++ src/WorkerTCP.java
@@ -1,2 +1,38 @@
-public class WorkerTCP {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class WorkerTCP implements Runnable {
+
+    private BufferedReader in;
+    private PrintWriter out;
+    private Socket s;
+
+    public WorkerTCP(Socket s) throws Exception {
+        this.in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        this.out = new PrintWriter(s.getOutputStream());
+        this.s = s;
+    }
+
+    public void run() {
+        String input;
+        while(true) {
+            try {
+                input=in.readLine();
+                if(input!=null && !input.equals("q")) {
+                    out.println("You typed: " + input);
+                    out.flush();
+                }
+                else {
+                    s.shutdownInput();
+                    s.shutdownOutput();
+                    s.close();
+                    break;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
