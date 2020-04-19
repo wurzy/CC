7eb39c6ebd30b36d59c83304ae5cdf8ad1ce3c7e
Válter <47797461+wurzy@users.noreply.github.com>
2020-04-19 23:50:04 +0100

    progress

--- src/SocketListener.java
+++ src/SocketListener.java
@@ -2,28 +2,46 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.List;
 
 public class SocketListener implements Runnable {
+    private final int MAXSIZE = 65536; // 64 kb
 
     private BufferedReader in;
     private PrintWriter out;
     private Socket s;
-    Arguments arguments;
+
+    private Arguments arguments;
+    private List<String> peers;
+    private String target;
+
+    private byte[] inBuffer;
+    private byte[] outBuffer;
+    private SocketWriter server;
+
 
     public SocketListener(Socket s, Arguments arg) throws Exception {
         this.in = new BufferedReader(new InputStreamReader(s.getInputStream()));
         this.out = new PrintWriter(s.getOutputStream());
         this.s = s;
         this.arguments = arg;
+
+        this.inBuffer = new byte[MAXSIZE];
+        this.outBuffer = new byte[MAXSIZE];
+
+        peers = arg.getArgumentsOf("peers");
+        target = arg.getArgumentsOf("target").get(0);
     }
 
     public void run() {
-        String input;
+        int len;
         try {
-
-            while ((input = in.readLine()) != null && !input.equals("q")) {
-                out.println("You typed: " + input);
-                out.flush();
+            while((len = s.getInputStream().read(inBuffer,0, MAXSIZE)) != -1) {
+                System.out.println("You typed: " + Arrays.toString(inBuffer));
+                outBuffer = Arrays.copyOf(inBuffer,len);
+                s.getOutputStream().write(outBuffer);
+                s.getOutputStream().flush();
             }
             s.shutdownInput();
             s.shutdownOutput();
